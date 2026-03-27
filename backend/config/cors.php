b1ec1677cf336ff8e5838a56c58766acfbb6fbfd
Gannnn <ganrongshen@gmail.com>
2026-03-27 19:44:23 +0800

    fix: cors origin issue

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -8,6 +8,8 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'https://boostorder.ganrongshen.com',
+        'https://boostorder-api.ganrongshen.com',
     ],
 
     'allowed_origins_patterns' => [],
