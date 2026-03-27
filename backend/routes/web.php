3fc8aa1385f0f4bf1fbb3881debc3f12c203d87f
Gannnn <ganrongshen@gmail.com>
2026-03-27 23:36:35 +0800

    fix: minor fix

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/storage/{path}', function (string $path) {
+    $file = storage_path('app/public/' . $path);
+    if (!file_exists($file)) {
+        abort(404);
+    }
+    return response()->file($file);
+})->where('path', '.*');
